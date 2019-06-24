6e18c422a23e0db757dcc995ce8cc8f9511f1063
lovisaakerstrom <50138740+lovisaakerstrom@users.noreply.github.com>
2019-06-24 21:42:59 +0200

    Update ProductActivity.java

--- app/src/main/java/se/juneday/systemetapp/ProductActivity.java
+++ app/src/main/java/se/juneday/systemetapp/ProductActivity.java
@@ -1,9 +1,11 @@
 package se.juneday.systemetapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
