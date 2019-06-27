c13f0de518032f417d96342cad20a1ae5ae94dbb
lovisaakerstrom <50138740+lovisaakerstrom@users.noreply.github.com>
2019-06-27 20:20:00 +0200

    Create CartActivity.java

---
+++ app/src/main/java/se/juneday/systemetapp/CartActivity.java
@@ -0,0 +1,22 @@
+package se.juneday.systemetapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CartActivity extends AppCompatActivity {
+
+
+    private Button empty_cart;
+    private TextView welcome_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+    }
+    }
